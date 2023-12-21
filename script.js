ce60d60a66284039739ce34ee3d016ecfa07e452
Frederick Ye <yenainglinn7@gmail.com>
2023-12-21 22:18:39 +0630

    fix navbar

--- script.js
+++ script.js
@@ -1,35 +1,21 @@
-document.querySelector('.navbar-toggler').addEventListener('click', () => {
-  document.querySelector('.navbar-toggler').classList.add('d-none');
-  document.getElementById('closeBtn').classList.remove('d-none');
-});
-
-document.getElementById('closeBtn').addEventListener('click', () => {
-  document.querySelector('.navbar-toggler').classList.remove('d-none');
-  document.getElementById('closeBtn').classList.add('d-none');
-});
-
-// const more = document.getElementById('moreBtn');
-// const less = document.getElementById('lessBtn');
-// const dynamicItems = document.querySelectorAll('.dynamic');
-
-// more.addEventListener('click', () => {
-//   dynamicItems.forEach(function(element) {
-//     element.classList.remove('hidden')
-//   });
-//   more.classList.add('hidden');
-//   less.classList.remove('hidden');
-// });
-
-// less.addEventListener('click', () => {
-//   dynamicItems.forEach(function(element) {
-//     element.classList.add('hidden')
-//   });
-//   more.classList.remove('hidden');
-//   less.classList.add('hidden');
-// });
 const dynamic = document.querySelector('.speaker-container');
 const more = document.getElementById('moreBtn');
 const less = document.getElementById('lessBtn');
+const openNavbar = document.querySelector('#open-btn');
+const closeNavbar = document.querySelector('.close-btn');
+const mobileNav = document.querySelector('.nav-bar-mobile');
+
+openNavbar.addEventListener('click', ()=> {
+  openNavbar.classList.add('d-none');
+  mobileNav.classList.remove('d-none');
+  document.body.style.overflow = 'hidden';
+})
+
+closeNavbar.addEventListener('click', ()=> {
+  openNavbar.classList.remove('d-none');
+  mobileNav.classList.add('d-none');
+  document.body.style.overflow = 'auto';
+})
 const speakers = [
   {
     Image: './images/speaker1.jpg',
